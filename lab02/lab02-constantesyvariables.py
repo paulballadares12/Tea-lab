263b06da58ad74dfdf1659d7959ead62632ecff9
Anthony Paul Balladares Lazo (24065) <anthony.balladares@est.zamorano.edu>
2022-09-11 21:57:35 -0600

    https://github.com/paulballadares12/Tea-lab.git

---
+++ lab02/lab02-constantesyvariables.py
@@ -0,0 +1,9 @@
+message = "Este es el laboratorio 2 de TEA"
+temperatura = 40.5
+pi = 3.14591654
+print(message)
+print(type(message))
+print(temperatura)
+temperatura = 45.1
+print(temperatura)
+print(type(temperatura))
